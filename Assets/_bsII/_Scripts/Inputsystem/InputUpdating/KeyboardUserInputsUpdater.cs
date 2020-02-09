23a214b757fdceb48cf933c7f21e4a062b0ee0cc
Alex B <broglale@students.zhaw.ch>
2020-02-09 18:53:31 +0100

    some folders and toggled input updater started

---
+++ Assets/_bsII/_Scripts/Inputsystem/InputUpdating/KeyboardUserInputsUpdater.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyboardUserInputsUpdater : IUserInputsUpdater
+{
+    public void UpdateUserInputs()
+    {
+        
+    }
+}
---
+++ Assets/_bsII/_Scripts/Inputsystem/LogicalUserinputs/ToggeledUserInput.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ToggeledUserInput
+{
+    public delegate void KeyUpEvent();
+    public event KeyUpEvent KeyUpEventEmitted;
+
+    public delegate void KeyDownEvent();
+    public event KeyDownEvent EmitKeyDownEventEmitted;
+
+    public bool IsPressed { get; private set; }
+
+}
