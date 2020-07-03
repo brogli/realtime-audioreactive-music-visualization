d8a8276a716cfb6569735d4dbea81ee2be8c9170
Alex B <broglale@students.zhaw.ch>
2020-07-03 07:48:37 +0200

    expand user inputs updater interface

--- Assets/_bsII/_Scripts/Inputsystem/InputUpdating/IUserInputsUpdater.cs
+++ Assets/_bsII/_Scripts/Inputsystem/InputUpdating/IUserInputsUpdater.cs
@@ -4,5 +4,30 @@ using UnityEngine;
 
 public interface IUserInputsUpdater
 {
-    void UpdateUserInputs();
+    #region volume related user inputs
+    void UpdateVolume();
+    #endregion
+
+    #region beat related user inputs
+    void UpdateFourInFour();
+    void UpdateTwoInFour();
+    void UpdateOneInFour();
+    void UpdateEightInFour();
+    void UpdateSixteenInFour();
+    #endregion
+
+    #region piano keys related user inputs
+    void UpdateDroneKeys();
+    void UpdateMelodyKeys();
+    void UpdateMoodKeys();
+    #endregion
+
+    #region other user inputs
+    void UpdateExplosionTriggers();
+    void UpdateGlobalIntensity();
+    #endregion
+
+    #region visual effects
+    void UpdateVisualEffects();
+    #endregion
 }
--- Assets/_bsII/_Scripts/Inputsystem/InputUpdating/KeyboardUserInputsUpdater.cs
+++ Assets/_bsII/_Scripts/Inputsystem/InputUpdating/KeyboardUserInputsUpdater.cs
@@ -4,8 +4,63 @@ using UnityEngine;
 
 public class KeyboardUserInputsUpdater : IUserInputsUpdater
 {
-    public void UpdateUserInputs()
+    public void UpdateDroneKeys()
     {
-        
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateEightInFour()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateExplosionTriggers()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateFourInFour()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateGlobalIntensity()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateMelodyKeys()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateMoodKeys()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateOneInFour()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateSixteenInFour()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateTwoInFour()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateVisualEffects()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void UpdateVolume()
+    {
+        throw new System.NotImplementedException();
     }
 }
