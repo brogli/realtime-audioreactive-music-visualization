ff160c725213f6f0c2751b91aa6c00f68e557c80
brogli <28339986+brogli@users.noreply.github.com>
2022-05-15 14:18:17 +0200

    process beatclock values and provide them in MusicValuesModel (#24)
    * process beatclock values and provide them in MusicValuesModel
    
    * cause extra commit
    
    closes #23

---
+++ Assets/_bsII/_Scripts/MusicValuesModel.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicValuesModel : MonoBehaviour
+{
+    #region beat values
+    public delegate void BeatEvent();
+    public event BeatEvent EmitFourInFourEvent;
+    private float fourInFourValue = 1.0f;
+
+    public float FourInFourValue
+    {
+        get => fourInFourValue;
+        set
+        {
+            if (fourInFourValue > 0.5 && value < 0.5)
+            {
+                EmitFourInFourEvent?.Invoke();
+            }
+            fourInFourValue = value;
+        }
+    }
+
+    public event BeatEvent EmitOneInFourEvent;
+    private float oneInFourValue = 1.0f;
+
+    public float OneInFourValue
+    {
+        get => oneInFourValue;
+        set
+        {
+            if (oneInFourValue > 0.5 && value < 0.5)
+            {
+                EmitOneInFourEvent?.Invoke();
+            }
+            oneInFourValue = value;
+        }
+    }
+
+    public event BeatEvent EmitTwoInFourEvent;
+    private float twoInFourValue = 1.0f;
+
+    public float TwoInFourValue
+    {
+        get => twoInFourValue;
+        set
+        {
+            if (twoInFourValue > 0.5 && value < 0.5)
+            {
+                EmitTwoInFourEvent?.Invoke();
+            }
+            twoInFourValue = value;
+        }
+    }
+
+    public event BeatEvent EmitEightInFourEvent;
+    private float eightInFourValue = 1.0f;
+
+    public float EightInFourValue
+    {
+        get => eightInFourValue;
+        set
+        {
+            if (eightInFourValue > 0.5 && value < 0.5)
+            {
+                EmitEightInFourEvent?.Invoke();
+            }
+            eightInFourValue = value;
+        }
+    }
+
+
+    public event BeatEvent EmitSixteenInFourEvent;
+    private float sixteenInFourValue = 1.0f;
+
+    public float SixteenInFourValue
+    {
+        get => sixteenInFourValue;
+        set
+        {
+            if (sixteenInFourValue > 0.5 && value < 0.5)
+            {
+                EmitSixteenInFourEvent?.Invoke();
+            }
+            sixteenInFourValue = value;
+        }
+    }
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
