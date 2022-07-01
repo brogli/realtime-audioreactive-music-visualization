c544c72dedda8000f54a052357e589e80c2c71d5
Alexander Brogli <alexanderbrogli@gmx.net>
2022-07-01 10:18:24 +0200

    Remove easings.net as source due to incompatible licence

--- Assets/_bsII/_Scripts/Helpers/Easings.cs
+++ Assets/_bsII/_Scripts/Helpers/Easings.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 /// <summary>
-/// Helper class providing common easings. Source: https://easings.net .
+/// Helper class providing common easings.
 /// </summary>
 public static class Easings
 {
@@ -15,6 +15,16 @@ public static class Easings
     public static float EaseInOutCubic(float x)
     {
         return x < 0.5f ? 4 * x * x * x : 1 - Mathf.Pow(-2 * x + 2, 3) / 2;
+    }
+
+    public static float EaseInExpo(float x)
+    {
+        return x == 0 ? 0 : Mathf.Pow(2, 10 * x - 10);
+    }
+
+    public static float EaseOutExpo(float x)
+    {
+        return x == 1 ? 1 : 1 - Mathf.Pow(2, -10 * x);
     }
 
 }
