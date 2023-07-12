22f77a9e896b27845b8ca5ae8bfd22c48851f769
RaphaelMaschinsen <raphaelmijnssen1@yahoo.de>
2023-07-13 01:27:42 +0200

    Sky Flight Scene first edition

--- Assets/_bsII/SceneSpecific/SkyFlightScene/SkyFlighSceneImplementation.cs
+++ Assets/_bsII/SceneSpecific/SkyFlightScene/SkyFlighSceneImplementation.cs
@@ -7,19 +7,30 @@ using UnityEngine.Rendering.HighDefinition;
 public class SkyFlighSceneImplementation : MonoBehaviour, IMusicInputsConsumer, IUserInputsConsumer
 {
     public GameObject cameraParent;
-    public AnimationCurve distCuve1;
+    public Camera mainCamera;
     public Volume PPVolume;
+    public Texture[] textureList;
+    public float fovDefault;
 
     public float _rotateValueX = 0.4f;
     public float _rotateValueY = 0.4f;
     public float _rotateValueZ = 0.5f;
 
+    public float rotValue = 0f;
+
     private UserInputsModel _userInputsModel;
     private MusicInputsModel _musicInputsModel;
 
     private Vector3 _camRotation;
+    private Explotion1 _explotion1;
+    private int _hdrTextureIndex = 0;
 
-    Bloom bloomComponent;
+
+    private Bloom _bloomComponent;
+    private HDRISky _hdriSkyComponent;
+    private ColorCurves _colorCurvesComponent;
+    private LensDistortion _lensDistortionComponent;
+    private PaniniProjection _paniniProjectionComponent;
 
     // Start is called before the first frame update
     void Start()
@@ -27,15 +38,37 @@ public class SkyFlighSceneImplementation : MonoBehaviour, IMusicInputsConsumer, 
         _userInputsModel = GameObject.FindGameObjectWithTag("UserInputsModel").GetComponent<UserInputsModel>();
         _musicInputsModel = GameObject.FindGameObjectWithTag("MusicInputsModel").GetComponent<MusicInputsModel>();
 
-        SubscribeMusicInputs();
-        SubscribeUserInputs();
-
         _camRotation = Vector3.zero;
 
-        if (PPVolume.profile.TryGet(out Bloom tmp))
+        if (PPVolume.profile.TryGet(out Bloom bloomComponentTmp))
         {
-            bloomComponent = tmp;
+            _bloomComponent = bloomComponentTmp;
         }
+
+        if (PPVolume.profile.TryGet(out HDRISky hdriSkyComponentTmp))
+        {
+            _hdriSkyComponent = hdriSkyComponentTmp;
+        }
+
+        if (PPVolume.profile.TryGet(out ColorCurves colorCurvesComponentTmp))
+        {
+            _colorCurvesComponent = colorCurvesComponentTmp;
+        }
+
+        if (PPVolume.profile.TryGet(out LensDistortion lensDistortionComponentTmp))
+        {
+            _lensDistortionComponent = lensDistortionComponentTmp;
+        }
+
+        if (PPVolume.profile.TryGet(out PaniniProjection paniniProjectionComponentTmp))
+        {
+            _paniniProjectionComponent = paniniProjectionComponentTmp;
+        }
+
+        _explotion1 = GetComponent<Explotion1>();
+
+        SubscribeMusicInputs();
+        SubscribeUserInputs();
     }
 
     // Update is called once per frame
@@ -56,36 +89,35 @@ public class SkyFlighSceneImplementation : MonoBehaviour, IMusicInputsConsumer, 
 
     private void HandleFourInFourMusicEvent()
     {
-        //_rotateValueX *= -1f;
-        //_rotateValueY *= -1f;
-        //_rotateValueZ *= -1f;
+        if (_userInputsModel.FourInFourUserInput.IsPressed)
+        {
+            int randomSign = Random.Range(0, 2) * 2 - 1;
+            _camRotation.z += rotValue * randomSign;
+        }
     }
 
     private void HandleTwoInFourMusicEvent()
     {
-
+        //fader only (fov zoom in)
     }
 
     private void HandleSixteenInFourMusicEvent()
     {
-
+        //fader only (dist1)
     }
 
     private void HandleEightInFourMusicEvent()
     {
-
+        //fader only (dist2)
     }
-
-
-
     private void HandleOneInEightMusicEvent()
     {
-
+        //fader only (dist3)
     }
 
     private void HandleOneInFourMusicEvent()
     {
-        if (_userInputsModel.FourInFourUserInput.IsPressed)
+        if (_userInputsModel.OneInFourUserInput.IsPressed)
         {
             _camRotation = Random.rotation.eulerAngles;
             cameraParent.transform.eulerAngles = _camRotation;
@@ -206,23 +238,17 @@ public class SkyFlighSceneImplementation : MonoBehaviour, IMusicInputsConsumer, 
         _userInputsModel.OneInFourUserInput.EmitTurnedOnOrOffEvent -= HandleOneInFourUserEvent;
     }
 
-    private void HandleOneInEightUserEvent(bool hasTurnedOn)
-    {
-        if (hasTurnedOn)
-        {
-        }
-        else
-        {
-        }
-    }
-
     private void HandleExplosionKeys(int index)
     {
         switch (index)
         {
             case 4:
-            case 6:
+                _explotion1.PlayCoroutine(1f, -90f);
+                break;
             case 5:
+                _explotion1.PlayCoroutine(1f, 18f);
+                break;
+            case 6:
             case 7:
             case 0:
             case 1:
@@ -236,18 +262,179 @@ public class SkyFlighSceneImplementation : MonoBehaviour, IMusicInputsConsumer, 
 
     private void HandleDroneKey(bool hasTurnedOn, int index)
     {
+        if (hasTurnedOn)
+        {
+            switch (index)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                default:
+                    break;
+            }
+        }
+        else if (!hasTurnedOn)
+        {
+            switch (index)
+            {
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                default:
+                    break;
+            }
+        }
     }
 
     private void HandleMoodKey(int index)
     {
+        switch (index)
+        {
+            // Switch HDR Texture and cam rotation
+            case 0:
+                int tmpTextureIndex;
+                do
+                {
+                    tmpTextureIndex = Random.Range(0, textureList.Length);
+                } while (tmpTextureIndex == _hdrTextureIndex);
+
+                _hdrTextureIndex = tmpTextureIndex;
+                _hdriSkyComponent.hdriSky.value = textureList[_hdrTextureIndex];
+                break;
+            // Pick Default HDR
+            case 1:
+                _hdrTextureIndex = 0;
+                _hdriSkyComponent.hdriSky.value = textureList[_hdrTextureIndex];
+                break;
+            // Random Slow Rotation
+            case 3:
+                _rotateValueX = Random.Range(-0.2f, 0.2f);
+                _rotateValueY = Random.Range(-0.2f, 0.2f);
+                _rotateValueZ = Random.Range(-0.5f, 0.5f);
+                break;
+            // Random Default Rotation
+            case 4:
+                _rotateValueX = Random.Range(-0.6f, 0.6f);
+                _rotateValueY = Random.Range(-0.6f, 0.6f);
+                _rotateValueZ = Random.Range(-1, 1f);
+                break;
+            // Random Fast Rotation
+            case 5:
+                _rotateValueX = Random.Range(-1.2f, 1.2f);
+                _rotateValueY = Random.Range(-1.2f, 1.2f);
+                _rotateValueZ = Random.Range(-2, 2f);
+                break;
+            // Random Crazy Rotation
+            case 6:
+                _rotateValueX = Random.Range(-5f, 5f);
+                _rotateValueY = Random.Range(-5f, 5f);
+                _rotateValueZ = Random.Range(-10, 10f);
+                break;
+            // Random Insane Rotation
+            case 7:
+                _rotateValueX = Random.Range(-20f, 20f);
+                _rotateValueY = Random.Range(-20f, 20f);
+                _rotateValueZ = Random.Range(-400, 400f);
+                break;
+            // Random Extremely Slow Rotation
+            case 2:
+                _rotateValueX = Random.Range(-0.08f, 0.08f);
+                _rotateValueY = Random.Range(-0.08f, 0.08f);
+                _rotateValueZ = Random.Range(-0.2f, 0.2f);
+                break;
+            default:
+                break;
+        }
     }
 
     private void HandleMelodyKey(bool hasTurnedOn, int index)
     {
+        if (hasTurnedOn)
+        {
+            switch (index)
+            {
+                case 0:
+                    _colorCurvesComponent.master.overrideState = true;
+                    break;
+                case 1:
+                    _colorCurvesComponent.red.overrideState = true;
+                    break;
+                case 2:
+                    _colorCurvesComponent.blue.overrideState = true;
+                    break;
+                case 3:
+                    _colorCurvesComponent.green.overrideState = true;
+                    break;
+                case 4:
+                    _colorCurvesComponent.hueVsSat.overrideState = true;
+                    break;
+                case 5:
+                    _colorCurvesComponent.satVsSat.overrideState = true;
+                    break;
+                case 6:
+                    _colorCurvesComponent.lumVsSat.overrideState = true;
+                    break;
+                case 7:
+                    _colorCurvesComponent.hueVsHue.value.AddKey(0.5f, Random.Range(-1f, 1f));
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (!hasTurnedOn)
+        {
+            switch (index)
+            {
+                case 0:
+                    _colorCurvesComponent.master.overrideState = false;
+                    break;
+                case 1:
+                    _colorCurvesComponent.red.overrideState = false;
+                    break;
+                case 2:
+                    _colorCurvesComponent.blue.overrideState = false;
+                    break;
+                case 3:
+                    _colorCurvesComponent.green.overrideState = false;
+                    break;
+                case 4:
+                    _colorCurvesComponent.hueVsSat.overrideState = false;
+                    break;
+                case 5:
+                    _colorCurvesComponent.satVsSat.overrideState = false;
+                    break;
+                case 6:
+                    _colorCurvesComponent.lumVsSat.overrideState = false;
+                    break;
+                case 7:
+                    _colorCurvesComponent.hueVsHue.value.RemoveKey(0);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     private void HandleVolumeUserEvent(bool hasTurnedOn)
     {
+        if (hasTurnedOn)
+        {
+            _paniniProjectionComponent.active = true;
+        }
+        else
+        {
+            _paniniProjectionComponent.active = false;
+        }
     }
 
     private void HandleLfVolumeUserEvent(bool hasTurnedOn)
@@ -256,10 +443,38 @@ public class SkyFlighSceneImplementation : MonoBehaviour, IMusicInputsConsumer, 
 
     private void HandleSixteenInFourUserEvent(bool hasTurnedOn)
     {
+        if (hasTurnedOn)
+        {
+            _lensDistortionComponent.intensity.value = 1f;
+        }
+        else if (!_userInputsModel.EightInFourUserInput.IsPressed && !_userInputsModel.OneInEightUserInput.IsPressed)
+        {
+            _lensDistortionComponent.intensity.value = 0f;
+        }
     }
 
     private void HandleEightInFourUserEvent(bool hasTurnedOn)
     {
+        if (hasTurnedOn)
+        {
+            _lensDistortionComponent.intensity.value = 1f;
+        }
+        else if (!_userInputsModel.SixteenInFourUserInput.IsPressed && !_userInputsModel.OneInEightUserInput.IsPressed)
+        {
+            _lensDistortionComponent.intensity.value = 0f;
+        }
+    }
+
+    private void HandleOneInEightUserEvent(bool hasTurnedOn)
+    {
+        if (hasTurnedOn)
+        {
+            _lensDistortionComponent.intensity.value = 1f;
+        }
+        else if (!_userInputsModel.SixteenInFourUserInput.IsPressed && !_userInputsModel.EightInFourUserInput.IsPressed)
+        {
+            _lensDistortionComponent.intensity.value = 0f;
+        }
     }
 
     private void HandleFourInFourUserEvent(bool hasTurnedOn)
